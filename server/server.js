1c2bf0a4b571a0562cac04706d1a0739e7880988
Jose <josemiguel.alves@altran.com>
2017-10-08 16:27:23 +0100

    GET /todos

--- server/server.js
+++ server/server.js
@@ -18,6 +18,14 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((result) => {
+        res.status(200).send({result});
+    }, (err) => {
+        res.status(400).send({err});
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 })
